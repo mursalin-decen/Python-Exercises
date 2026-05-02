a2e9cf39cb5e0b52237d85d1cc32959c56113c40
Mr Stranger <mursalinzam10@gmail.com>
2026-05-02 19:36:09 +0600

    Added Chapter 1 Variables problems

---
+++ Chapter one/Problem2.py
@@ -0,0 +1,5 @@
+#  Install an external module and use it to perform an operation of your interest. 
+import pyttsx3;
+engine = pyttsx3.init();
+engine.say("Hello guys , It's Me zamzam and oneday i will be a Hacker");
+engine.runAndWait();
