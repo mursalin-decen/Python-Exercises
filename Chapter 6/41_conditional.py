71b93ca6beb3c6b88be06208703b9e175d40a915
Mr Stranger <mursalinzam10@gmail.com>
2026-05-04 21:22:52 +0600

    Just updated File number one by one

---
+++ Chapter 6/41_conditional.py
@@ -0,0 +1,11 @@
+#ধর তোমারে বলছে যে ভাই তোমারে একটা কাজ করা লাগবে যদি কেও ১৮ বছরের নিছে এই ওয়েব সাইটে ঢুকে অরে বলবা
+#ভাই তুমি ১৮ এর নিচে আর যারা বড় তাদের বলবা ওয়েলকাম। 
+age = int(input("Enter your age: "))
+
+if(age>= 18):
+    print("\"WELCOME\"")
+
+else:
+    print("Sorry, You are under 18")
+
+print("Captcha Verification END")
